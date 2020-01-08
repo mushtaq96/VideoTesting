af408366d8513e1c2384b3f7eb90a39c68a0550d
mushtaq96 <mushtaq96smb@gmail.com>
2020-01-08 22:29:23 +0900

    add color frame instead of grayscale

--- open_webcam.py
+++ open_webcam.py
@@ -6,9 +6,11 @@ cap = cv2.VideoCapture(0)
 while(True):
 	ret,frame = cap.read()
 
-	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	#gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	colorr = cv2.cvtColor(frame,cv2.COLOR_RGB2BGR)
 
-	cv2.imshow('frame',gray)
+	#cv2.imshow('frame',gray)
+	cv2.imshow("",colorr)
 
 	if cv2.waitKey(1) & 0xFF ==ord('q'): 
 		break
